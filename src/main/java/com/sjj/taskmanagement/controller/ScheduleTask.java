847e6bb5e922a6c52806aad806ae4eb9550d20a9
Goshic <2069762101@qq.com>
2023-11-27 10:04:58 +0800

    m

--- src/main/java/com/sjj/taskmanagement/controller/ScheduleTask.java
+++ src/main/java/com/sjj/taskmanagement/controller/ScheduleTask.java
@@ -20,9 +20,8 @@ public class ScheduleTask extends BaseController {
 
     @Autowired
     SysUserMapper sysUserMapper;
-
     @Scheduled(cron="0 59 23 ? * 7")
-    private void settlement(){
+    public void settlement(){
         //强制所有人签退
         sysRegisterService.finishAllRegister();
         //扣除时长不达标人员的分
@@ -32,17 +31,22 @@ public class ScheduleTask extends BaseController {
             sysUserMapper.update(item,Wrappers.<SysUser>lambdaQuery().eq(SysUser::getUsername,item.getUsername()));
         }
         //邮箱发送不达标人员名单
-        String[] receivers=sysUserMapper.getAdminUserEmail().toArray(new String[0]);
-        sysRegisterService.sendResult(receivers);
-        //清空签到时长
-        sysRegisterService.clearAllRegister();
+        try {
+//            String[] receivers = sysUserMapper.getAdminUserEmail().toArray(new String[0]);
+            String[] receivers = {"2069762101@qq.com","1049175775@qq.com"};
+            sysRegisterService.sendResult(receivers);
+        }catch (Exception e){
+        }finally {
+            //清空签到时长
+            sysRegisterService.clearAllRegister();
+        }
     }
 
-    @Scheduled(cron="50 59 23 * * ?")
-    private void finishAllRegister(){
-        //强制所有人签退
-        sysRegisterService.finishAllRegister();
-    }
+//    @Scheduled(cron="50 59 23 * * ?")
+//    private void finishAllRegister(){
+//        //强制所有人签退
+//        sysRegisterService.finishAllRegister();
+//    }
 
     @Scheduled(cron="20 10 8 ? * 3")
     private void Test(){
--- src/test/java/com/sjj/taskmanagement/TaskmanagementApplicationTests.java
+++ src/test/java/com/sjj/taskmanagement/TaskmanagementApplicationTests.java
@@ -1,43 +1,23 @@
 package com.sjj.taskmanagement;
 
-import cn.hutool.json.JSON;
-import cn.hutool.json.JSONUtil;
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
-import com.baomidou.mybatisplus.core.toolkit.Wrappers;
-import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
-import com.sjj.taskmanagement.common.dto.MyPage;
-import com.sjj.taskmanagement.common.dto.MyPage;
-import com.sjj.taskmanagement.common.entities.*;
+import com.sjj.taskmanagement.common.entities.SysMenu;
+import com.sjj.taskmanagement.common.entities.SysRole;
+import com.sjj.taskmanagement.common.entities.SysUser;
 import com.sjj.taskmanagement.controller.BaseController;
-import com.sjj.taskmanagement.controller.VoteContriller;
-import com.sjj.taskmanagement.mapper.SysRegisterMapper;
+import com.sjj.taskmanagement.controller.ScheduleTask;
 import com.sjj.taskmanagement.mapper.SysUserMapper;
-import com.sjj.taskmanagement.mapper.VoteCheakMapper;
-import com.sjj.taskmanagement.mapper.VoteMapper;
-import com.sjj.taskmanagement.service.*;
-import com.sjj.taskmanagement.utils.MinioUtils;
-import com.sjj.taskmanagement.utils.RedisUtil;
-import io.minio.Result;
-import io.minio.errors.*;
-import io.minio.messages.Bucket;
-import io.minio.messages.Item;
+import com.sjj.taskmanagement.service.SysMenuService;
+import com.sjj.taskmanagement.service.SysRegisterService;
+import com.sjj.taskmanagement.service.SysRoleService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.transaction.annotation.Transactional;
 
 import javax.annotation.Resource;
-import java.io.IOException;
-import java.io.InputStream;
-import java.net.URLDecoder;
-import java.security.InvalidKeyException;
-import java.security.NoSuchAlgorithmException;
-import java.time.LocalDateTime;
-import java.util.ArrayList;
-import java.util.Iterator;
+import java.util.Arrays;
 import java.util.List;
-import java.util.Optional;
+import java.util.stream.Collectors;
 
 @SpringBootTest
 class TaskmanagementApplicationTests extends BaseController {
@@ -45,10 +25,17 @@ class TaskmanagementApplicationTests extends BaseController {
     SysRegisterService sysRegisterService;
     @Resource
     SysUserMapper sysUserMapper;
+    @Autowired
+    SysRoleService sysRoleService;
 
+    @Autowired
+    SysMenuService sysMenuService;
+
+    @Autowired
+    ScheduleTask scheduleTask;
     @Test
     public void test() {
-
+        scheduleTask.settlement();
     }
 
 }
